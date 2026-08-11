263d5dbed131afa64000d976e8e11ad106967182
chandima <chandimawithanage99@gmail.com>
2026-08-11 18:37:56 +0530

    Add initial Python files

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,8 +1,34 @@
+import sys
+
 import pygame
 
-pygame.init()
+from settings import Settings
 
-print("pygame is working!")
-print("pygame version:" ,pygame.version.ver)
+def run_game():
+    #Initialize pygame, settings and  screen object.
+    pygame.init()
+    ai_settings = Settings()
+    
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width
+    ))
+    pygame.display.set_caption("Alien Invasion")
+    
+    #set the bg color.
+    bg_color =(230,230,230)
+    
+    #Start the main loop for the game. 
+    while True:
+        
+        #watch for keybord and mouse events.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        
+        # redraw the screen during each pass through the loop.
+        screen.fill(bg_color)
+                
+        # make the most recently drawn screen visible.
+        pygame.display.flip()
 
-pygame.quit()
+run_game()
