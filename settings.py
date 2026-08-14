0fd64eede4feeea71ff5901436be705bd9b4481b
chandima <chandimawithanage99@gmail.com>
2026-08-14 17:07:26 +0530

    add setting python file

--- settings.py
+++ settings.py
@@ -1,14 +1,9 @@
 class Settings():
-    
     """a class to store all settings  for game"""
     
-    
     def __init__(self):
-        
         """Initialize the game's settings"""
-        
         # screen settings
-        self.screen_width = 1200
-        self.screen_height = 800
-        self.bg_color = (230,230,230)
-            
+        self.screen_width =1200
+        self.screen_height =800
+        self.bg_color =(230,230,230)
