3991c6f677dcc5879e10ce365df71c465683e63f
chandima <chandimawithanage99@gmail.com>
2026-08-15 14:14:44 +0530

    allowing continuous movement
    give it  flag attributes call moving_right and update method to check the status of the moving_right flag.

--- ship.py
+++ ship.py
@@ -17,6 +17,13 @@ class Ship():
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
         
+        # movement flag
+        self.moving_right = False
+        
+    def update(self):
+        """ update the ship's postion based on the movement flag."""
+        if self.moving_right:
+            self.rect.centerx += 1        
         
     def blitme(self):
         """Draw the ship at its current location."""
