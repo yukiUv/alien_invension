d0e57ee084881e2f742d364aea2ad0dd3da33ac5
chandima <chandimawithanage99@gmail.com>
2026-08-15 14:46:45 +0530

    update ship.py
    moving left side

--- ship.py
+++ ship.py
@@ -19,11 +19,14 @@ class Ship():
         
         # movement flag
         self.moving_right = False
+        self.moving_left = False
         
     def update(self):
         """ update the ship's postion based on the movement flag."""
         if self.moving_right:
-            self.rect.centerx += 1        
+            self.rect.centerx += 1     
+        if self.moving_left:
+            self.rect.centerx -= 1   
         
     def blitme(self):
         """Draw the ship at its current location."""
