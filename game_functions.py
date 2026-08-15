0a5bbca6ea97558c901d37735f3e1c7f7c9d82e5
chandima <chandimawithanage99@gmail.com>
2026-08-15 14:52:31 +0530

    modify the check events function in game_functions.py
    update  KEYDOWN AND KEYUP event for the left key

--- game_functions.py
+++ game_functions.py
@@ -11,10 +11,18 @@ def check_events(ship):
             if event.key == pygame.K_RIGHT:
                 # move the ship to the right
                 ship.moving_right = True
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                ship.moving_left = True
+                
         
         elif event.type == pygame.KEYUP:
-            if event.type == pygame.K_RIGHT:
+            if event.key == pygame.K_RIGHT:
                 ship.moving_right = False
+                
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                ship.moving_left = False
             
 def update_screen(ai_settings,screen,ship):
     """Update images on the screen and flip to the new screen"""
