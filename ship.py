54c9202f56bdc96c42486cb4dca6e9ca21ba6d4e
chandima <chandimawithanage99@gmail.com>
2026-08-16 12:36:03 +0530

    make some modifications to ship.py
    add ai_settings to the parameters for __init__ then update line 9,22,32,34 and 37

--- ship.py
+++ ship.py
@@ -2,10 +2,11 @@ import pygame
 
 class Ship():
     
-    def __init__(self,screen):
+    def __init__(self,ai_settings,screen):
         """initialize the ship and set its starting positions"""
         
         self.screen = screen
+        self.ai_settings = ai_settings
         
         # load the ship image and get its rect.
         self.image =pygame.image.load('image/ship.bmp')
@@ -17,16 +18,23 @@ class Ship():
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
         
+        # store a decimal value for the ship's center.
+        self.center = float(self.rect.centerx)
+        
         # movement flag
         self.moving_right = False
         self.moving_left = False
         
     def update(self):
         """ update the ship's postion based on the movement flag."""
+        # update the ship's cenetr value, not the rect.
         if self.moving_right:
-            self.rect.centerx += 1     
+            self.center += self.ai_settings.ship_speed_factor
         if self.moving_left:
-            self.rect.centerx -= 1   
+            self.center -= self.ai_settings.ship_speed_factor
+            
+        # update rect object from self.center.
+        self.rect.centerx = self.center   
         
     def blitme(self):
         """Draw the ship at its current location."""
