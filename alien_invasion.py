fbc28e398c50121a713484a9df804742b9143050
chandima <chandimawithanage99@gmail.com>
2026-08-18 21:06:54 +0530

    Update alian_invansion.py
    connect bullets functions line 27 and update line 25 : gf.check_events() function

--- alien_invasion.py
+++ alien_invasion.py
@@ -22,9 +22,10 @@ def run_game():
     
     #Start the main loop for the game. 
     while True:
-        gf.check_events(ship,bullets,ai_settings,screen)
+        gf.check_events(ai_settings,screen,ship,bullets)
         ship.update()
-        gf.update_screen(ai_settings,screen,ship)
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings,screen,ship,bullets)
         
         
         #watch for keybord and mouse events.
