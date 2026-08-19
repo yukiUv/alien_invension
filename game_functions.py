07413731be4ab168b754864294d9f5cb4e87757f
chandima <chandimawithanage99@gmail.com>
2026-08-19 17:23:38 +0530

    modify game_functions
    create_fleet function get to top of the code

--- game_functions.py
+++ game_functions.py
@@ -3,6 +3,23 @@ import pygame
 from bullet import Bullet
 from alien import Alien
 
+
+def create_fleet(ai_settings,screen,aliens):
+    """Create full fleet of the aliens."""
+    #spacing between each alien is equal to one alien width.
+    alien = Alien(ai_settings,screen)
+    alien_width = alien.rect.width
+    available_space_x = ai_settings.screen_width - 2 * alien_width
+    number_aliens_x = int(available_space_x / (2 * alien_width))
+    
+    # create the first row of aliens.
+    for alien_number in range(number_aliens_x):
+        #create an alien and place it in the row .
+        alien = Alien(ai_settings,screen)
+        alien.x = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        aliens.add(alien)
+
 def check_keydown_events(event,ai_settings,screen,ship,bullets):
     """ respond to key presses"""
     if event.key == pygame.K_RIGHT:
@@ -42,12 +59,13 @@ def update_screen(ai_settings,screen,ship,aliens,bullets):
     """Update images on the screen and flip to the new screen"""
     # redraw the screen during each pass through the loop.
     screen.fill(ai_settings.bg_color)
+    #draw the ship
     ship.blitme()
+    aliens.draw(screen)
     # Redraw all bullets behind ship and aliens.
     for bullet in bullets.sprites():
         bullet.draw_bullet()
-        ship.blitme()
-        aliens.draw(screen)
+        
 
 def update_bullets(bullets):
     """ Update position of bullets and get rid of old bullets."""
@@ -60,21 +78,7 @@ def update_bullets(bullets):
             bullets.remove(bullet)
             
 
-def create_fleet(ai_settings,screen,aliens):
-    """Create full fleet of the aliens."""
-    #spacing between each alien is equal to one alien width.
-    alien = Alien(ai_settings,screen)
-    alien_width = alien.rect.width
-    available_space_x = ai_settings.screen_width - 2 * alien_width
-    number_aliens_x = int(available_space_x / (2 * alien_width))
-    
-    # create the first row of aliens.
-    for alien_number in range(number_aliens_x):
-        #create an alien and place it in the row .
-        alien = Alien(ai_settings,screen)
-        alien.x = alien_width + 2 * alien_width * alien_number
-        alien.rect.x = alien.x
-        aliens.add(alien)
+
     
     # make the most recently drawn screen visible.
     pygame.display.flip()
