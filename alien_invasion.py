610432fdbd57aa890556c4b54ac64e7a9d49292c
chandima <chandimawithanage99@gmail.com>
2026-08-19 16:33:04 +0530

    Update alien_invasion.py
    connect to the alien

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from alien import Alien
 import game_functions as gf
 from pygame.sprite import Group
 
@@ -14,19 +15,24 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
     #set the bg color.
     
-    # make s ship
+    # make s ship, group of bullets , and a group of aliens.
     ship = Ship(ai_settings,screen)
-    # Make a group to store bullets in.
     bullets = Group()
+    aliens = Group()
+    
+    # create the fleet of an aliens.
+    gf.create_fleet(ai_settings,screen,aliens)
+    
+    
     
     
     #Start the main loop for the game. 
     while True:
         gf.check_events(ai_settings,screen,ship,bullets)
         ship.update()
-        gf.update_bullets(bullets)
-        gf.update_screen(ai_settings,screen,ship,bullets)
         
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings,screen,ship,aliens,bullets)
         
         #watch for keybord and mouse events.
         for event in pygame.event.get():
