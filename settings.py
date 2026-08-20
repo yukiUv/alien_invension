8aed345c41d87f0ac1e48d70de3922ba7f9721e3
chandima <chandimawithanage99@gmail.com>
2026-08-20 15:11:46 +0530

    modify settings.py
    change resolution on screen and change bullets speed

--- settings.py
+++ settings.py
@@ -5,12 +5,12 @@ class Settings():
         """Initialize the game's settings"""
         # screen settings
         self.screen_width =1200
-        self.screen_height =800
+        self.screen_height =720
         self.bg_color =(230,230,230)
         #ship settings
-        self.ship_speed_factor = 1.5
+        self.ship_speed_factor = 2.5
         # bullet settings
-        self.bullet_speed_factor = 1
+        self.bullet_speed_factor = 3
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = 60,60,60
