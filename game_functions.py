1125db5e0968bf996c45afa755d657609ab8c9c6
chandima <chandimawithanage99@gmail.com>
2026-08-20 15:10:31 +0530

    Update game_functions.py
    add get_number_row function and fix some bug

--- game_functions.py
+++ game_functions.py
@@ -7,26 +7,37 @@ from alien import Alien
 def get_number_aliens_x(ai_settings,alien_width):
     """Determine the number of aliens that fit in a row."""
     available_space_x = ai_settings.screen_width - 2 * alien_width
-    number_alien_x - int(available_space_x / (2 * alien_width))
-    return get_number_aliens_x
+    number_aliens_x = int(available_space_x / (2 * alien_width))
+    return number_aliens_x
+
+def get_number_rows(ai_settings,ship_height,alien_height):
+    """determine the number of rows of aliens that fit on the screen."""
+    available_space_y = (ai_settings.screen_height - (6 * alien_height)-ship_height)
+    number_rows = int(available_space_y / (2 * alien_height))
+    return number_rows
+
     
-def create_alien(ai_settings,screen,aliens,alien_number):
+def create_alien(ai_settings,screen,aliens,alien_number,row_number):
     """create an alien and place it in the row """
     alien = Alien(ai_settings,screen)
     alien_width = alien.rect.width
     alien_x = alien_width + 2 * alien_width * alien_number
-    alien.rect.x = alien.X
-    aliens.add(alien
-               )
-def create_fleet(ai_settings,screen,aliens):
+    alien.rect.x = alien_x
+    alien.rect.y = alien.rect.height + 2* alien.rect.height * row_number
+    aliens.add(alien)
+    
+def create_fleet(ai_settings,screen,ship,aliens):
     """Create full fleet of the aliens."""
     #create an alien and find the number of aliens in a row 
     alien = Alien(ai_settings,screen)
-    get_number_aliens_x = get_number_aliens_x(ai_settings,alien.rect.width)
+    number_aliens_x = get_number_aliens_x(ai_settings,alien.rect.width)
+    number_rows = get_number_rows(ai_settings,ship.rect.height,alien.rect.height)
     
-    # create the first row of aliens.
-    for alien_number in range(number_aliens_x):
-        create_alien(ai_settings,screen,aliens,alien_number)
+    #create the fleet of aliens.
+    for row_number in range(number_rows):
+        for alien_number in range(number_aliens_x):
+            create_alien(ai_settings,screen,aliens,alien_number,
+                         row_number)
 
 def check_keydown_events(event,ai_settings,screen,ship,bullets):
     """ respond to key presses"""
@@ -67,12 +78,15 @@ def update_screen(ai_settings,screen,ship,aliens,bullets):
     """Update images on the screen and flip to the new screen"""
     # redraw the screen during each pass through the loop.
     screen.fill(ai_settings.bg_color)
+     # Redraw all bullets behind ship and aliens.
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
     #draw the ship
     ship.blitme()
     aliens.draw(screen)
-    # Redraw all bullets behind ship and aliens.
-    for bullet in bullets.sprites():
-        bullet.draw_bullet()
+    
+  # make the most recently drawn screen visible.
+    pygame.display.flip()   
         
 
 def update_bullets(bullets):
@@ -88,5 +102,4 @@ def update_bullets(bullets):
 
 
     
-    # make the most recently drawn screen visible.
-    pygame.display.flip()
+  
