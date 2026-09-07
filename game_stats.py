0603200d8a01871dfa86bcfffee97b3a45901237
chandima <chandimawithanage99@gmail.com>
2026-09-07 21:20:00 +0530

    Update
    update start game in an inactive state

--- game_stats.py
+++ game_stats.py
@@ -5,8 +5,8 @@ class GameStats():
         """Initialize statistics."""
         self.ai_settings = ai_settings
         self.reset_stats()
-        # start alien invasion in an active state.
-        self.game_active = True
+        # start game in an inactive state
+        self.game_active = False
         
     def reset_stats(self):
         """Initialize statistics that can change during the game."""
