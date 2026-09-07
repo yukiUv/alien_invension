d5f69d50da851b1a05441ee25c60fdcdaa366535
chandima <chandimawithanage99@gmail.com>
2026-09-07 14:37:55 +0530

    update
    add  game_active flag as an attribute to GameStats to end the game.

--- game_stats.py
+++ game_stats.py
@@ -5,9 +5,11 @@ class GameStats():
         """Initialize statistics."""
         self.ai_settings = ai_settings
         self.reset_stats()
+        # start alien invasion in an active state.
+        self.game_active = True
         
     def reset_stats(self):
         """Initialize statistics that can change during the game."""
         self.ships_left = self.ai_settings.ship_limit
         
-        
+    
