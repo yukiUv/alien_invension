7b52a7eda8c1d6480bddf52bb488beb73ec15d40
chandima <chandimawithanage99@gmail.com>
2026-09-07 22:46:03 +0530

    modifying game_function file
     increase the game tempo by calling increase_speed and resetting the speed .

--- game_functions.py
+++ game_functions.py
@@ -84,6 +84,8 @@ def check_play_button(ai_settings,screen,stats,play_button,ship,aliens,bullets,m
     """Start a new game when the player clicks play."""
     button_clicked = play_button.rect.collidepoint(mouse_x,mouse_y)
     if button_clicked and not stats.game_active:
+        # reset the game settings
+        ai_settings.initialize_dynamic_settings()
         # hide the mouse cursor.
         pygame.mouse.set_visible(False)
         # reset the game statistics.
@@ -133,8 +135,9 @@ def check_bullets_alien_collisions(ai_settings,screen,ship,aliens,bullets):
     collisions = pygame.sprite.groupcollide(bullets,aliens,True,True)
     
     if len(aliens) == 0:
-        # destroy exsiting bullets and create new fleet.
+        # destroy exsiting bullets,speedup game  and create new fleet.
         bullets.empty()
+        ai_settings.increase_speed()
         create_fleet(ai_settings,screen,ship,aliens)
 
 def check_fleet_edges(ai_settings,aliens):
