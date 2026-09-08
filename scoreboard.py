3362e806ddb164997c02099143572bfedb125373
chandima <chandimawithanage99@gmail.com>
2026-09-08 23:11:58 +0530

    Update scoreboard file
    displaying the score and rounding the score

--- scoreboard.py
+++ scoreboard.py
@@ -7,12 +7,13 @@ class Scoreboard():
         """initialize scorekeeping attributes"""
         
         self.screen = screen
+        self.screen_rect = screen.get_rect()
         self.ai_settings = ai_settings
         self.stats = stats
         
         # font settings for scoring info
         self.text_color = (30,30,30)
-        self.font =pygame.font.SysFont(None,48)
+        self.font =pygame.font.SysFont(None,38)
         
         # prepare the initial score image.
         self.prep_score()
@@ -20,12 +21,12 @@ class Scoreboard():
     def prep_score(self):
         """ turn the score into a rendered image."""
         
-        score_str = str(self.stats.score)
-        self.score_image = self.font.render(score_str,True,self.text_color.self.ai_settings.bg_color)
+        rounded_score = int(round(self.stats.score, -1 ))
+        score_str = "{:,}" . format(rounded_score)
+        self.score_image = self.font.render(score_str,True,self.text_color,self.ai_settings.bg_color)
         
         # display the score at the top right of the screen.
-        
-        self.score_rect = self.score_image.get_rect
+        self.score_rect = self.score_image.get_rect()
         self.score_rect.right = self.screen_rect.right - 20
         self.score_rect.top = 20
         
