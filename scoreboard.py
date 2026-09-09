05654eaf62b49f4ced67a012b477537829ab5a43
chandima <chandimawithanage99@gmail.com>
2026-09-09 21:28:32 +0530

    adding ship life

--- scoreboard.py
+++ scoreboard.py
@@ -1,4 +1,7 @@
 import pygame.font
+from pygame.sprite import Group
+
+from ship import Ship
 
 
 class Scoreboard():
@@ -20,6 +23,7 @@ class Scoreboard():
         self.prep_score()
         self.prep_high_score()
         self.prep_level()
+        self.prep_ships()
 
     def prep_score(self):
         """Turn the current score into a rendered image."""
@@ -62,12 +66,26 @@ class Scoreboard():
         self.screen.blit(self.high_score_img, self.high_score_rect)
         self.screen.blit(self.level_img,self.level_rect)
         
+        # draw ships
+        self.ships.draw(self.screen)
+        
     def prep_level(self):
         # turn the level into a rendered image .
         self.level_img = self.font.render(str(self.stats.level), True,self.text_color,self.ai_settings.bg_color)
         
         # position the level below the score.
         self.level_rect = self.level_img.get_rect()
+        self.level_rect.right = self.score_rect.right
         self.level_rect.top = self.score_rect.bottom + 10
+    
+    def prep_ships(self):
+        """show how many ships are left """
+        
+        self.ships = Group()
+        for ship_number in range(self.stats.ships_left):
+            ship = Ship(self.ai_settings,self.screen)
+            ship.rect.x = 10 + ship_number * ship.rect.width
+            ship.rect.y = 10
+            self.ships.add(ship)
         
         
