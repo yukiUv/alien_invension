484842a9329290599db77369446f1848c0d2ed31
chandima <chandimawithanage99@gmail.com>
2026-09-09 19:59:52 +0530

    call sb to check_events

--- alien_invasion.py
+++ alien_invasion.py
@@ -6,7 +6,7 @@ from alien import Alien
 import game_functions as gf
 from pygame.sprite import Group
 from game_stats import GameStats
-from button import Button
+from button import Button    
 from scoreboard import Scoreboard
 
 def run_game():
@@ -37,7 +37,7 @@ def run_game():
     
     #Start the main loop for the game. 
     while True:
-        gf.check_events(ai_settings, screen,stats,play_button, ship,aliens, bullets)
+        gf.check_events(ai_settings, screen,stats,sb,play_button, ship,aliens, bullets)
         if stats.game_active:
             ship.update()
             gf.update_bullets(ai_settings,screen,stats,sb,ship,aliens,bullets)
